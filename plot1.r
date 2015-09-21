8bd4f41d0e56c12c61c171016376bfc9e47063e8
aksheyp <aksheyp@hotmail.com>
2015-09-21 18:03:27 +0800

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -5,7 +5,7 @@ png(filename = "C:/Users/Neena/Desktop/Coursera/NEI_data/plot1.png",
 +     width = 480, height = 480, 
 +     units = "px")
 
-totalEmissions <- aggregate(ND$Emissions, list(ND$year), FUN = "sum")
+totalEmissions <- aggregate(NEI$Emissions, list(NEI$year), FUN = "sum")
 
 plot(totalEmissions, type = "l", xlab = "Year", 
 +      main = "Total Emissions in the US between the years 1999 - 2008", 
